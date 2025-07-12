326e48c53976f9101261baafcca6a7b434f19fce
DrumClock <64655979+DrumClock@users.noreply.github.com>
2025-07-12 15:37:13 +0200

    Add files via upload

--- Keypad_tucnak/tucnak_keypad/button_sequences.h
+++ Keypad_tucnak/tucnak_keypad/button_sequences.h
@@ -1,6 +1,6 @@
 /*
 Example:
-#define BUT_1 "ALT+M, #300, CTRL+V, ALT+F4, END"
+#define BUT_1 "ALT+m, #300, CTRL+v, ALT+F4, END"
 
 Supported:
 - ALT+X,  CTRL+X, SHIFT+X, ALT+F4, CTRL+ENTER, ALT+TAB
@@ -21,12 +21,16 @@ https://tucnak.nagano.cz/wiki/Hotkeys
 
 */
 
-#define BUT_1  "ALT+M"    // přepíná režimy CW/SSB
-#define BUT_2  "ALT+B"    // přepne pásmo
-#define BUT_3  "ALT+C"    // prohodí značku v nedokončeném spojení
-#define BUT_4  "ALT+V"    // prohodí lokátor v nedokončeném spojení
-#define BUT_5  "CTRL+A"   // otočí první rotátor na aktuální azimut
-#define BUT_6  "CTRL+B"   // otočí druhý rotátor na aktuální azimut
+/* pozor na CTRL+M nebo CTRL+m 
+ - pří CTRL+M se odesílá "CTRL + SHIT + M" a některé programy s tím mají problém
+*/
+
+#define BUT_1  "ALT+m"    // přepíná režimy CW/SSB
+#define BUT_2  "ALT+b"    // přepne pásmo
+#define BUT_3  "ALT+c"    // prohodí značku v nedokončeném spojení
+#define BUT_4  "ALT+v"    // prohodí lokátor v nedokončeném spojení
+#define BUT_5  "CTRL+a"   // otočí první rotátor na aktuální azimut
+#define BUT_6  "CTRL+b"   // otočí druhý rotátor na aktuální azimut
 
 // nevyužité funkce
 
@@ -34,12 +38,12 @@ https://tucnak.nagano.cz/wiki/Hotkeys
 #define BUT_8  "ALT+1"         // Okno Spojení 
 #define BUT_9  "ALT+7"         // Okno KST chat
 #define BUT_10 "ALT+9"         // Okno Mapy
-#define BUT_11 "ALT+O, #300, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, O, P, T, "         // změna operátora 
-#define BUT_12 "ALT+O, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, M, F, G, ENTER"     // změna operátora MFG
-#define BUT_13 "ALT+O, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, J, O, C, ENTER"     // změna operátora JOC
-#define BUT_14 "ALT+O, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, A, P, A, ENTER"     // změna operátora APA
-#define BUT_15 "ALT+O, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, Z, J, H, ENTER"     // změna operátora ZJH
-#define BUT_16 "ALT+O, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, V, I, G, ENTER"     // změna operátora VIG
+#define BUT_11 "ALT+o, #300, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, O, P, T, "         // změna operátora 
+#define BUT_12 "ALT+o, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, M, F, G, ENTER"     // změna operátora MFG
+#define BUT_13 "ALT+o, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, J, O, C, ENTER"     // změna operátora JOC
+#define BUT_14 "ALT+o, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, A, P, A, ENTER"     // změna operátora APA
+#define BUT_15 "ALT+o, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, Z, J, H, ENTER"     // změna operátora ZJH
+#define BUT_16 "ALT+o, BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE,BACKSPACE, O, K, NUM1, V, I, G, ENTER"     // změna operátora VIG
 
 
 const char* SEQUENCES[] = {
@@ -48,4 +52,3 @@ const char* SEQUENCES[] = {
   BUT_9, BUT_10, BUT_11, BUT_12,
   BUT_13, BUT_14, BUT_15, BUT_16
 };
-
